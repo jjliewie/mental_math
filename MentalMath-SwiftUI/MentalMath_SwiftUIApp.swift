35cb9dd3b5ea8c84ecc8c8d9bd6415a5f2c3e069
Juheon <julierhee05@icloud.com>
2021-10-20 11:17:41 +0800

    Initial Commit

---
+++ MentalMath-SwiftUI/MentalMath_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MentalMath_SwiftUIApp.swift
+//  MentalMath-SwiftUI
+//
+//  Created by julie rhee on 2021/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MentalMath_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
