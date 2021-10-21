e35905c341fe83daa6703c89fc1393350e5ccc44
Juheon <julierhee05@icloud.com>
2021-10-21 18:25:34 +0800

    first commit

---
+++ MentalMath-SwiftUI/AddResultView.swift
@@ -0,0 +1,20 @@
+//
+//  AddResultView.swift
+//  MentalMath-SwiftUI
+//
+//  Created by julie rhee on 2021/10/21.
+//
+
+import SwiftUI
+
+struct AddResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddResultView()
+    }
+}
